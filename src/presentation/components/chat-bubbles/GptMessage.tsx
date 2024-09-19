edc9559141113c4393965efc11dd09db09b0ca0f
ariel.duarte <arielpy@gmail.com>
2024-09-19 15:19:09 -0300

    minor change

--- src/presentation/components/chat-bubbles/GptMessage.tsx
+++ src/presentation/components/chat-bubbles/GptMessage.tsx
@@ -14,7 +14,7 @@ export const GptMessage = ({ text, content }: Props) => {
       suggestions: [],
     };
   return (
-    <div className="col-start-1 col-end-12 p-3 rounded-lg">
+    <div className="col-start-1 col-end-8 p-3 rounded-lg">
       <div className="flex flex-row items-start">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shrink-0">
           IA
